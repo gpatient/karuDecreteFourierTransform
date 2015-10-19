6746f70588b38782714acd9fced12fa366700e35
gpatient <gpatient@naver.com>
2015-10-20 05:21:40 +0900

    initial

---
+++ index.js
@@ -0,0 +1,36 @@
+
+/**
+ * @module karuDecreteFourierTransform
+ * @name karuDecreteFourierTransform
+ * @license gpatient
+ * @author gpatient
+ * @version 0.0.001
+ */
+ 
+ 
+ export default karuDecreteFourierTransform;
+ import speech from 'gpatient/base64test';
+ import dbg from 'debug';
+
+var speecha = speech();   
+var qqq=speecha.qwer();
+dbg('karuDecreteFourierTransform ')(qqq.rrrr);
+
+
+function karuDecreteFourierTransform(){
+  if (!(this instanceof karuDecreteFourierTransform)) return new karuDecreteFourierTransform();
+}
+karuDecreteFourierTransform.prototype.qwer=function(){
+
+
+
+this.dsp=function(t){
+  var out=0;
+  out=qqq.dsp(t);
+  //return t%0.01*100; 
+  return out;
+};
+return this;
+
+};
+
---
+++ test.js
@@ -0,0 +1,18 @@
+
+/**
+ * test
+ */
+import speech from './index.js';
+  import dbg from 'debug';
+  
+  
+var speecha = speech();   
+var qqq=speecha.qwer();
+dbg('karuDecreteFourierTransform ')(qqq.rrrr);
+
+export function dsp(t)
+{
+  //return qqq.dsp(t);
+  return qqq.dsp(t);
+  //return Math.sin(300*t*3.141592*2);
+}
