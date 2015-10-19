d1dbcc26ab837ebb9b9cccdba3af633e4dc22fea
gpatient <gpatient@naver.com>
2015-10-20 05:59:05 +0900

    save3

--- index.js
+++ index.js
@@ -4,30 +4,92 @@
  * @name karuDecreteFourierTransform
  * @license gpatient
  * @author gpatient
- * @version 0.0.002
+ * @version 0.0.005
  */
  
  
  export default karuDecreteFourierTransform;
  import speech from 'gpatient/base64test';
- import dbg from 'debug';
+  import dbg from 'debug';
 
 var speecha = speech();   
 var qqq=speecha.qwer();
-dbg('karuDecreteFourierTransform ')(qqq.rrrr);
+dbg('karuDecreteFourierTransformaa ')(qqq.rrrr);
 
 
 function karuDecreteFourierTransform(){
-  if (!(this instanceof karuDecreteFourierTransform)) return new karuDecreteFourierTransform();
+  if (!(this instanceof karuDecreteFourierTransform)) 
+  return new karuDecreteFourierTransform();
 }
-karuDecreteFourierTransform.prototype.qwer=function(){
-var rrrr='qwer 5'
+karuDecreteFourierTransform.prototype.asdf=function(){
+this.ver='asdf pp';
+var tau=3.141592*2;
+var kdft={
+  tableMain:0,
+  tables:0,
+  tableLen:100,
+  resultTable:0,
+  mainCode:function(){
+   function initTable(tlen)
+   {
+	   this.tableMain= new Array(new Float32Array(tlen),new Float32Array(tlen));
+	   this.tables=new Array(2);
+	   this.resultTables=new Array(2);
+	   
+	   var i=0,j=0;
+	   this.tables[0]=new Array(tlen);
+	   this.tables[1]=new Array(tlen);
+	   for(i=0;i<tlen;i++){
+		this.tables[0][i]=new Float32Array(tlen);
+		this.tables[1][i]=new Float32Array(tlen);
 
+		}
+	   for(i=0;i<tlen;i++){
+		tableMain[0][i]=Math.sin(i*tau/tlen);
+		tableMain[1][i]=Math.cos(i*tau/tlen);
+	   }
+	   for(j=0;j<tlen;j++){
+		var pos=0;
+		for(i=0;i<tlen;i++){
+		 tables[0][j][i]=tableMain[0][pos];
+		 tables[0][j][i]=tableMain[0][pos];
+		 pos+=(j+1);
+		}
+	   }
+    }
+	function kdft(buf)
+	{
+		var i,j,sumA,sumB;
+		var blen=buf.length;
+		var pp=0,pos=0;
+		while(pos+tableLen<blen){
+		for(i=0;i<tableLen;i++){
+			sumA=0;
+			sumB=0;
+			for(j=0;j<tableLen;j++){
+				sumA+=buf[j+pos]*tables[0][i][j];
+				sumB+=buf[j+pos]*tables[1][i][j];
+			}
+			this.resultTable[0][i][pp]=sumA/tableLen;
+			this.resultTable[1][i][pp]=sumB/tableLen;
+			
+		}
+		pp++;
+		pos+=tableLen;
+		}
+		return kdft;
+	}
+	function sampleEnd(){}
+  },
+endObject:0
+
+};
 
 this.dsp=function(t){
   var out=0;
   out=qqq.dsp(t);
   //return t%0.01*100; 
+  //out=Math.sin(t*300*6);
   return out;
 };
 return this;
--- test.js
+++ test.js
@@ -2,17 +2,22 @@
 /**
  * test
  */
-import speech from './index.js';
+import karuDecreteFourierTransform from './index.js';
   import dbg from 'debug';
   
   
-var speecha = speech();   
-var qqq=speecha.qwer();
-dbg('karuDecreteFourierTransform ')(qqq.rrrr);
+var speecha = karuDecreteFourierTransform();   
+var qqq=speecha.asdf();
+
+var tt=new Array(1);
+tt[20]=0.123;
+tt[22]=0.124;
+tt[23]=0.125;
+dbg('karuDecreteFourierTransformbb ')(qqq.ver+ " "+tt[23]);
 
 export function dsp(t)
 {
   //return qqq.dsp(t);
   return qqq.dsp(t); 
   //return Math.sin(300*t*3.141592*2);
-}
+}  
